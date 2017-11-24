65a173b7126d9e4b99ec2389e222f8732e78a0ca
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-11-25 08:50:55 +1100

    issues controller, basic nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
+# == Route Map
+#
+#     Prefix Verb   URI Pattern                Controller#Action
+#     issues GET    /issues(.:format)          issues#index
+#            POST   /issues(.:format)          issues#create
+#  new_issue GET    /issues/new(.:format)      issues#new
+# edit_issue GET    /issues/:id/edit(.:format) issues#edit
+#      issue GET    /issues/:id(.:format)      issues#show
+#            PATCH  /issues/:id(.:format)      issues#update
+#            PUT    /issues/:id(.:format)      issues#update
+#            DELETE /issues/:id(.:format)      issues#destroy
+#
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => "pages#home"
+  # resources :users, :only => [:new]
+  resources :issues
 end
