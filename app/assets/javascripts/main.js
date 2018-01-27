a7f8c316a1f72d769e3e23b05e93960fbeffc822
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2018-01-27 16:27:39 +1100

    made multisearch bar responsive

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,3 +6,25 @@ const bgBlur = function () {
   const h = document.getElementsByClassName('main-heading')[0];
   h.className += ' blur';
 };
+
+
+
+// to shorten placeholder search text on smaller screens
+window.onload = function() {
+  smallScreenSearchPrompt();
+};
+
+window.onresize = function() {
+  smallScreenSearchPrompt();
+};
+
+
+
+const smallScreenSearchPrompt = function() {
+  const elem = document.getElementById('term');
+  if (window.innerWidth < 955) {
+    elem.placeholder = "Need help? Search here.";
+  } else {
+    elem.placeholder = "Need help?  You can ask me about an issue or a procedure you need to know.";
+  }
+};
